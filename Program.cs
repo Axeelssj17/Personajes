0368fae3c4e866f2cbf65600dc1afb3f49738d94
Axel Torres Marsula <atorresmarsula@gmail.com>
2025-05-21 19:40:34 -0300

    Actualizada 2

--- Program.cs
+++ Program.cs
@@ -31,117 +31,18 @@ class Personaje
     {
         Console.WriteLine($"Color: {Color}, Vida: {Vida}, Defensa: {Defensa}, Fuerza: {Fuerza}, Mana: {Mana}");
     }
-}
-
-class Programa
-{
-    static void Main()
-    {
-
-        Personaje jugador1 = new Personaje
-        {
-            Color = "Rojo",
-            Vida = 100,
-            Defensa = 10,
-            Fuerza = 20,
-            Mana = 50
-        };
-
-        Personaje jugador2 = new Personaje
-        {
-            Color = "Azul",
-            Vida = 120,
-            Defensa = 15,
-            Fuerza = 18,
-            Mana = 40
-        };
-
-        Console.WriteLine("Estado inicial: ");
-        jugador1.Mostrar();
-        jugador2.Mostrar();
-        Console.WriteLine();
-
-
-        jugador1.CambiarColor("Verde");
-        Console.WriteLine("Jugador 1 cambia de color a Verde: ");
-        jugador1.Mostrar();
-        Console.WriteLine();
-
-        jugador1.RecibirDaño(25);
-        Console.WriteLine("Jugador 1 recibe 25 de daño: ");
-        jugador1.Mostrar();
-        Console.WriteLine();
-
-        jugador1.Atacar(jugador2);
-        Console.WriteLine("Jugador 1 ataca a Jugador 2: ");
-        jugador1.Mostrar();
-        jugador2.Mostrar();
-        Console.WriteLine();
-
-        jugador1.RecibirDaño(200);
-        Console.WriteLine("Jugador 1 recibe daño letal: ");
-        jugador1.Mostrar();
-        if (jugador1.Vida <= 0)
-        {
-            Console.WriteLine("Jugador 1 ha sido derrotado");
-            Console.WriteLine("Opción: reiniciar o terminar");
-        }
-    }
-}
-
-class pocion : Personaje
-{
-
-    public int VidaMaxima { get; set; }
-    public int ManaMaximo { get; set; }
-
-    public pocion (string color, int vida, int defensa, int fuerza, int mana)
-    {
-        color = Color;
-        vida = Vida;
-        defensa = Defensa;
-        fuerza = Fuerza;
-        mana = Mana;
-        VidaMaxima = Vida;
-        ManaMaximo = Mana;
-    }
-
-    public void CambiarColor(string nuevoColor)
-    {
-        Color = nuevoColor;
-    }
-
-    public bool RecibirDaño(int daño)
-    {
-        int dañoFinal = daño - Defensa;
-        if (dañoFinal < 0) dañoFinal = 0;
-        Vida -= dañoFinal;
-        if (Vida < 0) Vida = 0;
-        return Vida > 0;
-    }
-
-    public bool Atacar(Personaje enemigo)
-    {
-        return enemigo.RecibirDaño(Fuerza);
-    }
 
     public void CurarVida(int cantidad)
     {
         Vida += cantidad;
-        if (Vida > VidaMaxima)
-            Vida = VidaMaxima;
+        Console.WriteLine($"Se curó {cantidad} puntos de vida");
     }
+
     public void CurarMana(int cantidad)
     {
         Mana += cantidad;
-        if (Mana > ManaMaximo)
-            Mana = ManaMaximo;
+        Console.WriteLine($"Se recuperó {cantidad} puntos de maná");
     }
-    public void Mostrar()
-    {
-        Console.WriteLine($"Color: {Color}, Vida: {Vida}/{VidaMaxima}, Defensa: {Defensa}, Fuerza: {Fuerza}, Mana: {Mana}/{ManaMaximo}");
-    }
-
 }
 
 abstract class Pocion
@@ -156,21 +57,56 @@ abstract class Pocion
         Minimo = minimo;
         Maximo = maximo;
     }
+
     public abstract void Usar(Personaje personaje);
-    class PocionVida : Pocion
+}
+
+class PocionVida : Pocion
+{
+    public PocionVida(int min, int max) : base(min, max) { }
+
+    public override void Usar(Personaje personaje)
     {
-        public PocionVida(int min, int max) : base(min, max) { }
+        int cantidad = rnd.Next(Minimo, Maximo + 1);
+        personaje.CurarVida(cantidad);
+    }
+}
 
-        public void Usar(Personaje personaje)
-        {
-            int cantidad = rnd.Next(Minimo, Maximo + 1);
-            personaje.CurarVida(cantidad);
-            Console.WriteLine($"Se curo {cantidad} puntos de vida");
-        }
+class PocionMana : Pocion
+{
+    public PocionMana(int min, int max) : base(min, max) { }
 
-        public override void Usar(Personaje personaje)
+    public override void Usar(Personaje personaje)
+    {
+        int cantidad = rnd.Next(Minimo, Maximo + 1);
+        personaje.CurarMana(cantidad);
+    }
+}
+
+class Programa
+{
+    static void Main()
+    {
+        Personaje jugador1 = new Personaje
         {
-            throw new NotImplementedException();
-        }
+            Color = "Rojo",
+            Vida = 50,
+            Defensa = 5,
+            Fuerza = 15,
+            Mana = 30
+        };
+
+        Console.WriteLine("Estado inicial del jugador:");
+        jugador1.Mostrar();
+        Console.WriteLine();
+
+        Pocion pocionVida = new PocionVida(10, 20);
+        pocionVida.Usar(jugador1);
+
+        Pocion pocionMana = new PocionMana(5, 15);
+        pocionMana.Usar(jugador1);
+
+        Console.WriteLine("\nEstado después de usar pociones:");
+        jugador1.Mostrar();
     }
 }
